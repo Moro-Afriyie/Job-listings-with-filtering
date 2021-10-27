22df71aa4a7684dbe5adf52d7f58fa730279778e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-27 15:17:23 +0000

    style the card

--- job-listings-with-filtering/src/components/Card.tsx
+++ job-listings-with-filtering/src/components/Card.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "../styles/Card.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ICardProps {}
@@ -6,14 +7,18 @@ interface ICardProps {}
 const Card: React.FunctionComponent<ICardProps> = (props) => {
   return (
     <div className="card__container">
-      <div className="card__avatar">
-        <h1>avatar</h1>
+      <div className="card__left">
+        <div className="card__avatar">
+          <h1>avatar</h1>
+        </div>
+        <div className="card__details">
+          <h1>details</h1>
+        </div>
       </div>
-      <div className="card__details">
-        <h1>details</h1>
-      </div>
-      <div className="card__tags">
-        <h1>tags</h1>
+      <div className="card__right">
+        <div className="card__tags">
+          <h1>tags</h1>
+        </div>
       </div>
     </div>
   );
