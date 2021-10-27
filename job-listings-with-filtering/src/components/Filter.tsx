1b3d2c5dab2aa4b9f2beef46955642bf5e503000
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-27 13:42:38 +0000

    style the filter

--- job-listings-with-filtering/src/components/Filter.tsx
+++ job-listings-with-filtering/src/components/Filter.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
+import "../styles/Filter.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IFilterProps {}
 
 const Filter: React.FunctionComponent<IFilterProps> = (props) => {
-  return <h1>Filter</h1>;
+  return (
+    <div className="filter">
+      <h1>Filter</h1>
+    </div>
+  );
 };
 
 export default Filter;
