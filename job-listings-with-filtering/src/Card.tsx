3c7d78d8420a4f8e4e058a0c03612a843351fcf6
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-27 10:50:33 +0000

    create filter and card components

---
+++ job-listings-with-filtering/src/Card.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ICardProps {}
+
+const Card: React.FunctionComponent<ICardProps> = (props) => {
+  return <h1>Card</h1>;
+};
+
+export default Card;
---
+++ job-listings-with-filtering/src/Filter.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IFilterProps {}
+
+const Filter: React.FunctionComponent<IFilterProps> = (props) => {
+  return <h1>Filter</h1>;
+};
+
+export default Filter;
