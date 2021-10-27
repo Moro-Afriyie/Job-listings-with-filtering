f9b3aff5bbe82efd137ed94a6207a95bc341530c
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-27 11:01:33 +0000

    import the various components in app.tsx

--- job-listings-with-filtering/src/App.tsx
+++ job-listings-with-filtering/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.scss";
+import Card from "./components/Card";
+import Filter from "./components/Filter";
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <div className="header"></div>
+      <Filter />
+      <div className="body">
+        <Card />
+      </div>
+    </main>
+  );
 }
 
 export default App;
