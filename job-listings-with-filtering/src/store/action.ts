d85e1a732fa8ed8bd6a6ecbff97fdcbd80829837
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-12 11:50:39 -0100

    add a payload to my filterJoblistingsAction

--- job-listings-with-filtering/src/store/action.ts
+++ job-listings-with-filtering/src/store/action.ts
@@ -3,5 +3,6 @@ import { FILTER_JOB_LISTINGS } from "./actionTypes";
 export const filterJObListingsAction = (data: string) => {
   return {
     type: FILTER_JOB_LISTINGS,
+    payload: data,
   };
 };
